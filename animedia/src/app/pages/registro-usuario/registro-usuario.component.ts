22afa9ecefcd254debeaaef11738b15c07618b6d
julianza32 <julianza32@gmail.com>
2020-06-23 15:52:13 -0500

    redireccionamiento de registro y login

--- animedia/src/app/pages/registro-usuario/registro-usuario.component.ts
+++ animedia/src/app/pages/registro-usuario/registro-usuario.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { Usuario } from '../../model/usuario';
-import { UsuarioService } from  '../../services/usuario.service'; 
+import { UsuarioService } from  '../../services/usuario.service';
+import {Router, ActivatedRoute,Params} from '@angular/router' 
 //'../../services/usuario.service';
 @Component({
   selector: 'app-registro-usuario',
   templateUrl: './registro-usuario.component.html',
   styleUrls: ['./registro-usuario.component.css']
 })
+
 export class RegistroUsuarioComponent implements OnInit {
   public usuarioModel: Usuario;
   public url:String;
 
-  constructor(private usuarioService: UsuarioService) {
+  @ViewChild('tyc') tyc: ElementRef;
+
+  constructor(private usuarioService: UsuarioService,private _router:Router) {
     this.usuarioModel= new Usuario('','','',0,new Date,'','','','');
    }
 
   ngOnInit(): void {
   }
+  validarFormulario(){
+  if(this.tyc.nativeElement.checked){
+    this.registrarUsuario();
+  }else{
+    alert("Por favor Acepta terminos y condiciones");
+  }
+  }
+
   registrarUsuario(){
     this.usuarioService.registarUsuario(this.usuarioModel).subscribe(
       (response:any)=>{
@@ -28,6 +40,7 @@ export class RegistroUsuarioComponent implements OnInit {
         }else{
           alert(`Te registraste correctamente! inicia sesion con ${usuario.email}`);
           this.usuarioModel= new Usuario('','','',0,new Date,'','','','');
+          this._router.navigate(['/home']);
         }
       }else{
           alert(response.messaje);
