a2c5b7b14a05adce4c1d3785b81eb02d3a3975e3
japerezcarri <54781818+japerezcarri@users.noreply.github.com>
2020-06-23 09:25:39 -0500

    boleteria

--- animedia/src/app/pages/boleteria/boleteria.component.ts
+++ animedia/src/app/pages/boleteria/boleteria.component.ts
@@ -12,7 +12,9 @@ export class BoleteriaComponent implements OnInit {
   columnas=[1,2,3,4,];
   columnas2=[5,6,7,8,9,10,11,12,13,14];
   columnas3=[15,16,17,18];
-  sillas=['A','B','C','D','E','F','G','H','I','J','K']
+  columnas4=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  sillas=['A','B','C','D','E','F','G','H']
+  sillas2=['I','J','K']
 
   ngOnInit(): void {
     
