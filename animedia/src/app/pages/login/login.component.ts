b6f57b4592f872409805dbbda55c590bf4141421
andrealsanchez2018 <andrealsanchez2018@gmail.com>
2020-06-23 00:03:28 -0500

    adelanto login

--- animedia/src/app/pages/login/login.component.ts
+++ animedia/src/app/pages/login/login.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public imagenLogo: string = "../../../assets/imagenes/logoAnimediabb.png"
+ /* https://giphy.com/gifs/l6mBchxYZc7Sw/html5 */
+  // public imagenLogo: file = "../../../assets/imagenes/logoAnimediabb.png"
+
+
   constructor() { }
 
   ngOnInit(): void {
