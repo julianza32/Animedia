67646b7915fb37aa9549b088682b82b265855c46
julianza32 <julianza32@gmail.com>
2020-06-27 17:21:40 -0500

    merge

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const path = require('path');
 
 //Función de registrar usuario
-function registerUser(req, res){
+function registerUser(req, res) {
     var user = new User();
     var parameters = req.body;
 
@@ -23,43 +23,42 @@ function registerUser(req, res){
     user.affiliateCard = parameters.affiliateCard;
 
     //Buscar el usuario apra encontrar is tiene imagen de perfil y borrarla
-    User.findOne({email : parameters.email},(err,userFound)=>{
-        if(err)
-        {
-            res.status(500).send({message:"Error en el servidor"});
-        }if(!userFound){
+    User.findOne({ email: parameters.email }, (err, userFound) => {
+        if (err) {
+            res.status(500).send({ message: "Error en el servidor" });
+        } if (!userFound) {
             //Función save() para interactuar con la DB
-            user.save((err, userNew)=>{
-                if(!userNew){
-                    res.status(200).send({message: "No fue posible realizar el registro"})
-                }else{
+            user.save((err, userNew) => {
+                if (!userNew) {
+                    res.status(200).send({ message: "No fue posible realizar el registro" })
+                } else {
                     res.status(200).send({
                         message: "Usuario creado",
                         user: userNew
                     });
                 }
-            });            
-        }else{
+            });
+        } else {
             res.status(200).send({
                 message: "El correo ya existe en nuestra plataforma"
             });
         }
-        
+
     });
 
-    
+
 
 }
 
 //Función de mostrar todos los usuarios
-function getAllUsers(req, res){
-    User.find((err, usersFound)=>{
-        if(err){
-            res.status(500).send({message: "Error en el servidor"});
-        }else{
-            if(!usersFound){
-                res.status(200).send({message:"No fue posible encontrar usuarios"});
-            }else{
+function getAllUsers(req, res) {
+    User.find((err, usersFound) => {
+        if (err) {
+            res.status(500).send({ message: "Error en el servidor" });
+        } else {
+            if (!usersFound) {
+                res.status(200).send({ message: "No fue posible encontrar usuarios" });
+            } else {
                 res.status(200).send({
                     message: "Usuarios encontrados",
                     users: usersFound
@@ -71,15 +70,15 @@ function getAllUsers(req, res){
 }
 
 //Función de mostrar usuario en especifico
-function getUser(req, res){
+function getUser(req, res) {
     var userId = req.params.id;
-    User.findById(userId, (err, userFound)=>{
-        if(err){
-            res.status(500).send({message: "Error en el servidor"});
-        }else{
-            if(!userFound){
-                res.status(200).send({message:"No fue posible encontrar el usuario"});
-            }else{
+    User.findById(userId, (err, userFound) => {
+        if (err) {
+            res.status(500).send({ message: "Error en el servidor" });
+        } else {
+            if (!userFound) {
+                res.status(200).send({ message: "No fue posible encontrar el usuario" });
+            } else {
                 res.status(200).send({
                     message: "Usuario encontrado",
                     user: userFound
@@ -91,19 +90,19 @@ function getUser(req, res){
 }
 
 //Función de actualizar usuario
-function updateUser(req, res){ 
+function updateUser(req, res) {
     var userId = req.params.id;
     var newUserData = req.body;
 
-    User.findByIdAndUpdate(userId, newUserData, (err, updatedUser)=>{
-        if(err){
-            res.status(200).send({message:"Error en el servidor"});
-        }else{
-            if(!updatedUser){
-                res.status(200).send({message:"No fue posible actualizar el usuario"});
-            }else{
+    User.findByIdAndUpdate(userId, newUserData, (err, updatedUser) => {
+        if (err) {
+            res.status(200).send({ message: "Error en el servidor" });
+        } else {
+            if (!updatedUser) {
+                res.status(200).send({ message: "No fue posible actualizar el usuario" });
+            } else {
                 res.status(200).send({
-                    message:"Usuario actualizado",
+                    message: "Usuario actualizado",
                     user: updatedUser
                 });
             }
@@ -112,39 +111,40 @@ function updateUser(req, res){
 }
 
 //Función de eliminar usuario
-function deleteUser(req, res){
-    var userId = req.params.id; 
+function deleteUser(req, res) {
+    var userId = req.params.id;
+    var imagen = req.params;
     var ruta = './files/users/';
 
+    console.log(req.params);
     //Buscar el usuario apra encontrar is tiene imagen de perfil y borrarla
-    User.findOne({_id : userId},(err,userFound)=>{
-        if(err)
-        {
-            res.status(500).send({message:"Error en el servidor"});
-        }else if(!userFound){
-            res.status(200).send({message:"Usuario inexistente"});
-        }else if(userFound.imagen = !null)
-        {
-            //borrar archivo de imagen
-            fs.unlink(ruta+userFound.image, err =>{
-                if (err) {
-                    res.status(200).send({message:`Error ${err}` });
-                } 
-            });
+    User.findOne({ _id: userId }, (err, userFound) => {
+        if (err) {
+            res.status(500).send({ message: "Error en el servidor" });
+        } else if (!userFound) {
+            res.status(200).send({ message: "Usuario inexistente" });
+            // }else if(userFound.image = !null)
+            // {
+            //     //borrar archivo de imagen
+            //     fs.unlink(ruta+userFound.image, err =>{
+            //         if (err) {
+            //             res.status(200).send({message:`Error ${err}` });
+            //         } 
+            //     });
             //fin de borrar archivo
         }
     });
 
 
-    User.findByIdAndDelete(userId, (err, deletedUser)=>{
-        if(err){
-            res.status(500).send({message: "Error en el servidor"});
-        }else{
-            if(!deletedUser){
-                res.status(200).send({message:"No fue posible eliminar el usuario"});
-            }else{
+    User.findByIdAndDelete(userId, (err, deletedUser) => {
+        if (err) {
+            res.status(500).send({ message: "Error en el servidor" });
+        } else {
+            if (!deletedUser) {
+                res.status(200).send({ message: "No fue posible eliminar el usuario" });
+            } else {
                 res.status(200).send({
-                    message:"Usuario eliminado",
+                    message: "Usuario eliminado",
                     user: deletedUser
                 });
             }
@@ -153,12 +153,12 @@ function deleteUser(req, res){
 }
 
 
-function login(req, res){
+function login(req, res) {
     var parametros = req.body;
     var emailUser = parametros.email;
     var passUser = parametros.pass;
 
-    User.findOne({ email: emailUser }, (err, userLogin) =>{
+    User.findOne({ email: emailUser }, (err, userLogin) => {
         if (err) {
             res.status(500).send({ message: "Error en el servidor" });
         } else {
@@ -180,19 +180,19 @@ function login(req, res){
 
 }
 
-function uploadUserImage(req, res){
-    
+function uploadUserImage(req, res) {
+
     var usuarioId = req.params.id;
     var nombreArchivo = "No has subido ninguna imagen...";
     var ruta = './files/users/';
-    console.log("intentando subir imagen"+req.files.image.name);
-   
+    console.log("intentando subir imagen" + req.files.image.name);
+
     //Validar si efectivamente se esta enviando un archivo
 
     if (req.files) {
         // Vamos a ir analizando la ruta del archivo, el nombre y la extención
         // C:\\usuarios\descargas\imagen.png
-        var rutaArchivo = req.files.image.path ;
+        var rutaArchivo = req.files.image.path;
         console.log(`Ruta archivo: ${rutaArchivo}`);
 
         // Haremos un split para separar elementos
@@ -214,49 +214,46 @@ function uploadUserImage(req, res){
         console.log(`Extension archivo: ${extensionArchivo}`);
 
         // Validar si el formato del archivo es aceptable 
-        if(extensionArchivo == 'png'||extensionArchivo=='jpg'||extensionArchivo=='jpeg'){
-            
+        if (extensionArchivo == 'png' || extensionArchivo == 'jpg' || extensionArchivo == 'jpeg') {
+
             //Buscar que hay en image si existe algún archivo lo elimina para dar paso al nuevo
-            User.findOne({_id: usuarioId},(err,userFound)=>{
-                if(err){
-                    
-                    res.status(500).send({message: "Error en el servidor"});
+            User.findOne({ _id: usuarioId }, (err, userFound) => {
+                if (err) {
+
+                    res.status(500).send({ message: "Error en el servidor" });
                 }
-                else if(userFound.image){
-                    console.log(ruta+userFound.image);
-                    fs.open(ruta+userFound.image,(err,data)=>{
-                        if(err)
-                        {
+                else if (userFound.image) {
+                    console.log(ruta + userFound.image);
+                    fs.open(ruta + userFound.image, (err, data) => {
+                        if (err) {
                             console.log("no se encontro el archivo");
                             //res.status(200).send({message:`Error crash ${err}` });
-                        }else if(!data)
-                        {
+                        } else if (!data) {
                             console.log("error de lectura");
                             //res.status(200).send({message:`Error lectura ${err}` });
-                        }else
-                        {
-                        //borrar archivo de imagen para actualizar
-                        fs.unlink(ruta+userFound.image, (error)=>{
-                        if(error){
-                            //res.status(200).send({message: });
-                            console.log(`Error ${error}`);
-                            
-                             }               
-                        }); 
+                        } else {
+                            //borrar archivo de imagen para actualizar
+                            fs.unlink(ruta + userFound.image, (error) => {
+                                if (error) {
+                                    //res.status(200).send({message: });
+                                    console.log(`Error ${error}`);
+
+                                }
+                            });
                         }
                     });
-                    
+
                 }
             });
-            
+
             //Actulizar del usuario el campo imagen
-            User.findByIdAndUpdate(usuarioId,{image:nombreArchivo},(err,usuarioConImg)=>{
-                if(err){
-                    res.status(500).send({message: "Error en el servidor"});
-                }else{
-                    if(!usuarioConImg){
-                        res.status(200).send({message: "No fue posible subir la imagen"});
-                    }else{
+            User.findByIdAndUpdate(usuarioId, { image: nombreArchivo }, (err, usuarioConImg) => {
+                if (err) {
+                    res.status(500).send({ message: "Error en el servidor" });
+                } else {
+                    if (!usuarioConImg) {
+                        res.status(200).send({ message: "No fue posible subir la imagen" });
+                    } else {
                         res.status(200).send({
                             message: "Imagen anexada",
                             image: nombreArchivo,
@@ -265,28 +262,28 @@ function uploadUserImage(req, res){
                     }
                 }
             });
-        }else{
-            res.status(200).send({message:"Formato invalido! El archivo no es una imagen"})
+        } else {
+            res.status(200).send({ message: "Formato invalido! El archivo no es una imagen" })
         }
     } else {
         res.status(200).send({ message: "No has subido imagen" });
     }
 }
 
-function getUserImage(req, res){
+function getUserImage(req, res) {
     //pedir el archivo que queros mostrar
     var archivo = req.params.imageFile;
     //ubicacion del archivo 
-    var ruta = './files/users/'+archivo;
+    var ruta = './files/users/' + archivo;
 
     //validar si existe o no 
     //fs.exists('la ruta del archivo a buscar, (existencia)=>{}')
 
-    fs.exists(ruta,(exists)=>{
-        if(exists){
+    fs.exists(ruta, (exists) => {
+        if (exists) {
             res.sendFile(path.resolve(ruta));
-        }else{
-            res.status(200).send({message: "Imagen no encontrada"});
+        } else {
+            res.status(200).send({ message: "Imagen no encontrada" });
         }
     });
 }
