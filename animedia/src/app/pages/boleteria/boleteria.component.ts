e2f68ff1e9d73ba2be95226bd8cde6f11cd35078
julianza32 <julianza32@gmail.com>
2020-06-30 12:42:26 -0500

    comunicacion service y pagos

--- animedia/src/app/pages/boleteria/boleteria.component.ts
+++ animedia/src/app/pages/boleteria/boleteria.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild,ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { PedidoService } from 'src/app/services/pedido.service';
+
 
 
 @Component({
@@ -8,34 +10,39 @@ import { Component, OnInit, ViewChild,ElementRef} from '@angular/core';
 })
 export class BoleteriaComponent implements OnInit {
 
-  public puestos=0;
-  public puestosE=0;
+  public puestos = 0;
+  public puestosE = 0;
 
-  constructor() { 
-   
+  constructor(public pedido: PedidoService) {
+
   }
-  columnas=[1,2,3,4,];
-  columnas2=[5,6,7,8,9,10,11,12,13,14];
-  columnas3=[15,16,17,18];
-  columnas4=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-  sillas=['A','B','C','D','E','F','G','H']
-  sillas2=['I','J','K']
+  columnas = [1, 2, 3, 4,];
+  columnas2 = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  columnas3 = [15, 16, 17, 18];
+  columnas4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  sillas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+  sillas2 = ['I', 'J', 'K']
 
   ngOnInit(): void {
     
   }
- /*  ngDoCheck(){
-    
-  } */
-  
-  status:boolean=false;
-puestosSeleccionados(x){
-  if (this.puestos<this.puestosE){
-    this.puestos++;
-    document.getElementById(x).setAttribute("class","btn btn-success sillas");
-  }else{
-    alert(`Ya escogiste tus puestos, puedes dirigirte a pagos ${this.puestosE}`);
+  /*  ngDoCheck(){
+     
+   } */
+
+  status: boolean = false;
+  puestosSeleccionados(x) {
+    if (this.puestos < this.puestosE) {
+      this.puestos++;
+      document.getElementById(x).setAttribute("class", "btn btn-success sillas");
+    } else {
+      alert(`Ya escogiste tus puestos, puedes dirigirte a pagos ${this.puestosE}`);
+    }
   }
-  
-}
+
+  pagos(){
+    this.pedido.producto = this.puestosE;
+    alert(this.pedido.producto);
+    
+  }
 }
