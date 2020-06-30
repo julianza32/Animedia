dc108651fe9702e6833590e64e62df82ee97e8af
julianza32 <julianza32@gmail.com>
2020-06-30 13:51:43 -0500

    Arreglo de enrutamiento entre componentes :v

--- animedia/src/app/services/pedido.service.ts
+++ animedia/src/app/services/pedido.service.ts
@@ -5,7 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class PedidoService {
 
-  public producto : any; 
+  public producto : []; 
 
   constructor() { }
 }
+
+/*
+nombre producto : Boleta(Jack)-Tarjeta platino - Combo 1
+imagen producto : imagen.png-imagen.jpg - imagenCombo.jpg
+descripcion : puestos/sala - 6 meses de membresia de platino -descripcion del combo 
+valor unitario: 12000 - 50000 - 20000
+cantidad: cantidad sillas - 1 (no editable) - 1 o + 
+*/
