5fe3130d5847f114e99140a19466bea0a73c9417
ShawnDFox <mararmlav@gmail.com>
2020-06-30 20:23:29 -0500

    chatbox + pagos de confiteria

--- animedia/src/app/components/chat-box/chat-box.component.ts
+++ animedia/src/app/components/chat-box/chat-box.component.ts
@@ -11,9 +11,33 @@ export class ChatBoxComponent implements OnInit {
   @ViewChild('max') maxi: ElementRef;
   @ViewChild('min') mini: ElementRef;
 
-  constructor(private renderer: Renderer2) { }
+  public preguntas=[
+    {"indice":1,"texto":"¿Como puedo ver contenido dentro de la pagina?"},
+    {"indice":2,"texto":"en donde puedo cambiar mi membresia"},
+    {"indice":3,"texto":"pregunta 3"}
+  ];
+  public respuesta:any;
+
+  constructor(private renderer: Renderer2) {
+    this.respuesta = [];
+   }
   
   ngOnInit(): void {
+  }
+
+  validarPregunta(id)
+  {
+    console.log(id);
+    
+    switch(id)
+    {
+      case 1: this.respuesta.push({"texto":"Debes iniciar sesion y contar con una cuenta platino"});
+      break;
+      case 2:this.respuesta.push({"texto":"Puedes seguir"+"<a routerLink='/tarjetas'>este link</a>"+" para hacer el cambio" });
+      break;
+      case 3:this.respuesta.push({"texto":" respuesta pregunta 2"});;
+      break;
+    }
   }
 
   maximizar() {
@@ -23,7 +47,7 @@ export class ChatBoxComponent implements OnInit {
     // alert("miximizo")
   }
   minimizar() {
-    this.renderer.setStyle(this.chat.nativeElement, "height","30px");
+    this.renderer.setStyle(this.chat.nativeElement, "height","0px");
     this.renderer.setStyle(this.mini.nativeElement,'display','none');
     this.renderer.setStyle(this.maxi.nativeElement,'display', 'flex');
   }
