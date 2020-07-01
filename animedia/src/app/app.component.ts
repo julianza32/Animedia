4d8f7dded8f4e664175ff8017cbcb68be5da3817
japerezcarri <54781818+japerezcarri@users.noreply.github.com>
2020-07-01 17:42:31 -0500

    estilos

--- animedia/src/app/app.component.ts
+++ animedia/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'animedia';
+
+  ngOnInit() {
+
+    let interval = window.setInterval(this.videoM, 10000);
+    /*  clearInterval(interval); */
+    let interval2 = window.setInterval(this.visibleAnimedia, 999);
+    let interval3 = window.setInterval(this.apearAnimedia, 9999);
+    document.getElementsByTagName('video')[0].play();
+  }
+  videoM() {
+
+
+    document.getElementById('intro').style.display = 'none';
+    document.getElementsByTagName('video')[0].pause();
+
+  }
+
+  visibleAnimedia() {
+    document.getElementById('animedia').setAttribute('class', 'animedia visible');
+  }
+  apearAnimedia() {
+
+    document.getElementById('animedia').style.transition = 'width 2s, height 2s, margin 2s';
+    document.getElementById('animedia').style.width = '100%';
+    document.getElementById('animedia').style.margin = '0';
+  }
+
 }
+
